4943272316860e38b50cffdc89806d626012aeb0
junseongkim <junseong@unitsoft.kr>
2021-09-10 14:51:55 +0900

    project start

---
+++ src/main/java/com/example/tictactoe_spring/TictactoeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tictactoe_spring/TictactoeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
